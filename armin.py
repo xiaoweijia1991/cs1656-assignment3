1156e6f608e22c29704dae720af770d84001e263
xiaoweijia1991 <jiaxx221@umn.edu>
2021-03-08 11:31:46 -0500

    Add files via upload

---
+++ armin.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from pandas import Series, DataFrame
+import itertools as it
+import pandas as pd
+import math
+import csv
+import sys
+import argparse
+import collections
+import glob
+import os
+import re
+import requests
+import string
+import sys
+
+class Armin():
+    
+    def apriori(self, input_filename, output_filename, min_support_percentage, min_confidence):
+        """
+        Implement the Apriori algorithm, and write the result to an output file
+
+        PARAMS
+        ------
+        input_filename: String, the name of the input file
+        output_filename: String, the name of the output file
+        min_support_percentage: float, minimum support percentage for an itemset
+        min_confidence: float, minimum confidence for an association rule to be significant
+
+        """
+        pass
+
+if __name__ == "__main__":
+    armin = Armin()
+    armin.apriori('input.csv', 'output.sup=0.5,conf=0.7.csv', 0.5, 0.7)
+    armin.apriori('input.csv', 'output.sup=0.5,conf=0.8.csv', 0.5, 0.8)
+    armin.apriori('input.csv', 'output.sup=0.6,conf=0.8.csv', 0.6, 0.8)
